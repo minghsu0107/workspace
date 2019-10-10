c747b993096cf6dae57d342bc67ddb35fc021d06
許浩鳴 <minghsu0107@gmail.com>
2019-10-11 01:39:13 +0800

    update

--- C/Special/grid.c
+++ C/Special/grid.c
@@ -13,7 +13,7 @@ int d(int x, int y, int dp[][105]){
       dp[0][i] = 1;
   }
   for (int i = 1; i < x; i++) {
-      for(int j = 1; j < y;j++) {
+      for(int j = 1; j < y; j++) {
          if (dp[i][j] == -1)continue;
          if (dp[i-1][j] != -1) dp[i][j] += dp[i-1][j];
          if (dp[i][j-1] != -1) dp[i][j] += dp[i][j-1];
